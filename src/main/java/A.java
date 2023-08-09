7b73f2df5af72a08d0b20b0a4e81db780c97a690
Sharook <sk.sharook106@gmail.com>
2023-08-09 23:39:26 +0530

    Attempt 1

---
+++ src/main/java/A.java
@@ -0,0 +1,6 @@
+public class A {
+    String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
+class A
+{
+    String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
+class B extends A {
+    String meth()
+    {
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        A a = new B();
+
+        System.out.println(a.meth());
+    }
 }
