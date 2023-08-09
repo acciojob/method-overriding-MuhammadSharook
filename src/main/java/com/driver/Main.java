e990e02202f32a7da0101d5ecfda635f4c900032
Sharook <sk.sharook106@gmail.com>
2023-08-09 23:52:11 +0530

    example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,13 +2,13 @@ package com.driver;
 
 class A
 {
-    String meth()
+    public String meth()
     {
         return "Invoking method from class A";
     }
 }
 class B extends A {
-    String meth()
+    public String meth()
     {
         return "Method is overridden in Extendend class B";
     }
